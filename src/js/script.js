ef25aa9f1d6a0b5ebf2f3a6e6ad9ba9d4a3c5f79
ayaka <aykaa.0724@gmail.com>
2024-01-27 21:43:17 +0900

    header（sp,pc）

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,14 @@
-
 jQuery(function ($) { // この中であればWordpressでも「$」が使用可能になる
 
+  //ドロワーメニュー
+  $(".js-hamburger").click(function () {
+    if ($(".js-hamburger").hasClass("is-active")) {
+      $(".js-hamburger").removeClass("is-active");
+      $(".js-sp-nav").fadeOut();
+    } else {
+      $(".js-hamburger").addClass("is-active");
+      $(".js-sp-nav").fadeIn();
+    }
+  });
 
 });
