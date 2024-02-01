eeed57437703bf548224b0136f001865fd508118
ayaka <aykaa.0724@gmail.com>
2024-02-01 22:06:48 +0900

    0201

--- src/js/script.js
+++ src/js/script.js
@@ -13,4 +13,32 @@ jQuery(function ($) { // この中であればWordpressでも「$」が使用可
     }
   });
 
+  $(function(){
+    var swiper = new Swiper(".js-campaign-swiper", {
+      loop: true,   // クルクル回ってる
+      slidesPerView: 1.26,    // 1画面にいくつ表示させるか
+      breakpoints: {
+        768: {
+          slidesPerView: 3.29,
+          spaceBetween: 30    // slider間の余白
+        },
+        1024: {
+          slidesPerView: 3.49,
+          spaceBetween: 35
+        }
+      },
+      spaceBetween: 24,
+      speed: 2000,
+      autoplay: {
+        delay: 1000,
+        disableOnInteraction: false
+      },
+      // Navigation arrows
+      navigation: {
+        prevEl: ".campaign-swiper__prev"
+        nextEl: ".campaign-swiper__next",
+      }
+    });
+  });
+  
 });
